3eaa94bffba94c5bd9370c6aee00aae882d29dcb
Michael Cooper <55610025+mcooper7649@users.noreply.github.com>
2020-04-16 15:01:08 -0400

    moved app to components folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Heading from "./components/Heading";
+import Heading from "./Heading";
 
 function App() {
   return (
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDom from "react-dom";
 
 function Heading() {
   const date = new Date();
